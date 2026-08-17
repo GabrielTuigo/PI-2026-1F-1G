40650f38647f29f770f1ca5c83ac6a8cac2f2c26
Gabriel815-Maker <gabrielhenriquedebritobutka@gmail.com>
2026-08-17 08:41:00 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,3 @@
-
-
-let trilho=document.getElementById('trilho')
-let body=document.querySelector('body')
-
-trilho.addEventListener("click", ()=>{
-  trilho.classList.toggle('dark')
-  body.classList.toggle('dark')
-})
+function alternarTema() {
+  document.body.classList.toggle("light");
+}
